8bb360017cb5af82c6c0d69980e01380c6caf18e
Mariya Kazakova <60565206+mary-asl@users.noreply.github.com>
2020-08-13 16:08:05 +0600

    change "Locale" class

--- src/test/java/com/epam/task5/tests/businessObject/Locale.java
+++ src/test/java/com/epam/task5/tests/businessObject/Locale.java
@@ -1,16 +1,11 @@
 package com.epam.task5.tests.businessObject;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Locale {
 
     private String country;
     WebDriver driver;
-    private static final By GEO_LOCATION_TEXT_LOCATOR = By.xpath("//span[@class='geo j-geocity-text']");
 
     public Locale(WebDriver driver) {
         this.driver = driver;
@@ -20,13 +15,8 @@ public class Locale {
         this.country = country;
     }
 
-    public String getCountry(){
+    public String getCountry() {
         return country;
     }
 
-    public WebElement getCurrentLocale() {
-        new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(GEO_LOCATION_TEXT_LOCATOR));
-        return driver.findElement(GEO_LOCATION_TEXT_LOCATOR);
-    }
 }

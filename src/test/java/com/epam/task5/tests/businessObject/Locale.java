eda84255558a8ad2c27a454699a14c33fb4bf987
Mariya Kazakova <60565206+mary-asl@users.noreply.github.com>
2020-08-13 16:28:25 +0600

    update Locale.java

--- src/test/java/com/epam/task5/tests/businessObject/Locale.java
+++ src/test/java/com/epam/task5/tests/businessObject/Locale.java
@@ -1,15 +1,8 @@
 package com.epam.task5.tests.businessObject;
 
-import org.openqa.selenium.WebDriver;
-
 public class Locale {
 
     private String country;
-    WebDriver driver;
-
-    public Locale(WebDriver driver) {
-        this.driver = driver;
-    }
 
     public void setCountry(String country) {
         this.country = country;
